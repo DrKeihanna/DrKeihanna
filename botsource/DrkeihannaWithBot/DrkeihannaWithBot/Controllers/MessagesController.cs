1204876542da8c7b3a1714f6568b630f78121d05
壮秀 徳田 <a-tokuda@kip.co.jp>
2017-07-22 18:16:15 +0900

    ボットのソース。 Dialogs/RootDialog.cs にif文で判断つけてます。 VS2017(.Net4.6)で開発。

---
+++ botsource/DrkeihannaWithBot/DrkeihannaWithBot/Controllers/MessagesController.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Autofac;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+using System.Configuration;
+using System;
+
+namespace DrkeihannaWithBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else
+            {
+                Activity reply = HandleSystemMessage(activity);
+                if (reply != null)
+                {
+                    ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // メンバー追加、削除といった会話ステータスの変更を把握
+                // Activity.MembersAdded, Activity.MembersRemoved,
+                // Activity.Action で詳細を確認
+                if (message.MembersAdded[0].Name == ConfigurationManager.AppSettings["BotId"].ToString())
+                {
+                    return message.CreateReply($"こんにちは！<br>調べたい研究を聞いてね！<br>今は「ロボット」「植物」「農業」について教えられるよ！");
+                }
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
